fb8e71f4bee44fd61f0c95238ea291c59ef9a6ce
was <zxt_zel_npl@163.com>
2018-01-04 17:59:59 +0800

    fix the query date

--- server/router.js
+++ server/router.js
@@ -6,47 +6,51 @@ const router = new Router()
 const moment = require('moment')
 
 
-router.get('/page', () => {
-
+router.get('/',async (ctx ,next) =>{
+  try{
+    let data = await readFile('./src/index.html')
+    ctx.response.type='html'
+    ctx.response.body = data
+  }
+  catch(err){
+    ctx.body = {message: err.message}
+    ctx.status = err.status || 500
+  }
 })
 
-router.get('/query',async (ctx,a) => {
+router.get('/query/:num',async (ctx) => {
   try {
-    console.log(ctx)
-    console.log(ctx.connection)
-    console.log(a)
-    console.log(this)
-    // let num = ctx.data.num
-    // let arr = []
-    // for (let i = 1; i <= num; i++) {
-    //   let obj
-    //   let time = moment().subtract(i, 'days').format('YYYY-MM-DD')
-    //   let str1 = string4(time)
-    //   let str2 = string2(time)
-    //   let str3 = string3(time)
-    //   if (i === 1) {
-    //     let results1 = await query(connection, str1)
-    //     let results2 = await query(connection, str2)
-    //     let results3 = await query(connection, str3)
-    //     obj = {
-    //       time,
-    //       results1: results1.length,
-    //       results2: results2[0]["COUNT(DISTINCT openid)"],
-    //       results3: results3[0]["COUNT(DISTINCT openid)"],
-    //     }
-    //   }
-    //   else {
-    //     let results1 = await query(connection, str1)
-    //     obj = {
-    //       time,
-    //       results1: results1.length,
-    //     }
-    //   }
-    //
-    //   arr.unshift(obj)
-    // }
-    // ctx.body = format_html(arr)
-    ctx.body = 'Hello world1'
+    let num = ctx.params.num
+    let connection = ctx.connection
+    let arr = []
+    for (let i = 1; i <= num; i++) {
+      let obj
+      let time = moment().subtract(i, 'days').format('YYYY-MM-DD')
+      let str1 = string4(time)
+      let str2 = string2(time)
+      let str3 = string3(time)
+      if (i === 1) {
+        let results1 = await query(connection, str1)
+        let results2 = await query(connection, str2)
+        let results3 = await query(connection, str3)
+        obj = {
+          time,
+          results1: results1.length,
+          results2: results2[0]["COUNT(DISTINCT openid)"],
+          results3: results3[0]["COUNT(DISTINCT openid)"],
+        }
+      }
+      else {
+        let results1 = await query(connection, str1)
+        obj = {
+          time,
+          results1: results1.length,
+        }
+      }
+
+      arr.unshift(obj)
+    }
+    ctx.body = format_html(arr)
   } catch (err) {
     ctx.body = {message: err.message}
     ctx.status = err.status || 500
@@ -70,18 +74,6 @@ router.get('/hello',async (ctx, next) => {
     // }
     ctx.body = 'Hello world'
   } catch (err) {
-    ctx.body = {message: err.message}
-    ctx.status = err.status || 500
-  }
-})
-
-router.get('/',async (ctx ,next) =>{
-  try{
-    let data = await readFile('./src/index.html')
-    ctx.response.type='html'
-    ctx.response.body = data
-  }
-  catch(err){
     ctx.body = {message: err.message}
     ctx.status = err.status || 500
   }

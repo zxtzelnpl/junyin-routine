31992f26bda95e2a779187dcb92896fab837cad0
was <zxt_zel_npl@163.com>
2018-01-09 16:25:51 +0800

    change cdata to false

--- server/libs/util.js
+++ server/libs/util.js
@@ -4,7 +4,7 @@ const fs=require('fs');
 const xml2js = require('xml2js')
 let build = new xml2js.Builder({
   rootName:'xml',
-  cdata:true
+  cdata:false
 })
 
 exports.readFileAsync = function(fpath,encoding){
